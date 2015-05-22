17055e7c479c7d5c7a01b2818549f44b99d8bff4
Brian Woodward <brian.woodward@gmail.com>
2015-05-21 23:41:32 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * github-pub-test-2 <https://github.com/doowb/github-pub-test-2>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
